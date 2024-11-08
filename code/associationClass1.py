0dff47844325a52a527de75d07f9fa6160a01630
willian <willianrelara@gmail.com>
2024-11-08 14:02:53 -0300

    commit uml

---
+++ code/associationClass1.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+class AssociationClass1:
+    pass
---
+++ code/class2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from MENU import MENU
+
+
+class Class2(MENU):
+    pass
