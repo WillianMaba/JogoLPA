51c2d03bd8c8c1913a3eaeb9ee854399861847ba
willian <willianrelara@gmail.com>
2024-11-08 15:08:49 -0300

    Creating background, music and text in the menu

--- code/game.py
+++ code/game.py
@@ -2,22 +2,19 @@
 # -*- coding: utf-8 -*-
 import pygame.display
 
+from code.const import WINDOW_WIDTH, WINDOW_HEIGHT
 from code.menu import Menu
 
 
 class Game:
     def __init__(self):
         pygame.init()
-        self.window = pygame.display.set_mode(size=(800, 640))
+        self.window = pygame.display.set_mode(size=(WINDOW_WIDTH, WINDOW_HEIGHT))
 
-    def run(self, ):
+    def run(self):
         while True:
             menu =Menu(self.window)
             menu.run()
             pass
 
-            #check for all events
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    quit()
+
